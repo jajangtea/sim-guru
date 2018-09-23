b64d209fcea1d2f5056eea9b3662751edf029df0
jj <jj@jajangtea>
2018-09-23 22:42:02 +0700

    riwayat tugas tambahan dan update db

--- protected/pages/m/default/riwayat/Pendidikan.php
+++ protected/pages/m/default/riwayat/Pendidikan.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php
 prado::using ('Application.pagecontroller.m.riwayat.CPendidikan');
 class Pendidikan extends CPendidikan {                
     public function onLoad($param) {
         parent::onLoad($param);                         
     }     
-=======
-<?php
-prado::using ('Application.pagecontroller.m.riwayat.CPendidikan');
-class Pendidikan extends CPendidikan {                
-    public function onLoad($param) {
-        parent::onLoad($param);                         
-    }     
->>>>>>> 5be9bc446bf9048c8c402cd34125dfda33620238
 }
--- protected/pages/m/default/setting/UserGuru.php
+++ protected/pages/m/default/setting/UserGuru.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php
 prado::using ('Application.pagecontroller.m.setting.CUserGuru');
 class UserGuru extends CUserGuru {    
 	public function onLoad($param) {		
 		parent::onLoad($param);		                                   
     }
-=======
-<?php
-prado::using ('Application.pagecontroller.m.setting.CUserGuru');
-class UserGuru extends CUserGuru {    
-	public function onLoad($param) {		
-		parent::onLoad($param);		                                   
-    }
->>>>>>> 5be9bc446bf9048c8c402cd34125dfda33620238
 }
